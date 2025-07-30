6b104abb0908251aa4b8dd0ddc0031c9f01f5caa
Bingeeverything <mr.0gamero5@gmail.com>
2025-07-31 09:39:54 +1000

    version 2

--- 704_Binary_Search.py
+++ 704_Binary_Search.py
@@ -1,7 +1,13 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        for i, val in enumerate(nums):
-            if val  == target:
-                return i
-        else:
-            return -1
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return -1 
