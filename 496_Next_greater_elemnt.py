880c91519a1c465fc7261fec847b36b6c6c3d64f
Bingeeverything <mr.0gamero5@gmail.com>
2025-09-30 21:09:15 +1000

    using a stack and dict, while theres something in stack, we pop it out and move to the next, otherwise add to it, if stack still exists, add tha5t value to the dic

--- 496_Next_greater_elemnt.py
+++ 496_Next_greater_elemnt.py
@@ -1,9 +1,14 @@
-    start_index = nums2.index(value)
-            found = False
-            for i in range(start_index + 1, len(nums2)):
-                if nums2[i] > value:
-                    yo.append(nums2[i])
-                    found = True
-                    break
-            if not found:
-                yo.append(-1)
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        stack = []
+        mydic = {}
+        for num in reversed(nums2):
+            while stack and stack[-1] <= num:
+                stack.pop
+                if stack:
+                    mydic[num] = stack[-1]
+                else:
+                    mydic[num] = -1
+                stack.append(num)
+            stack.append(mydic[num])
+        return stack                
